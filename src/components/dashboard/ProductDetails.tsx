f8dda7c55f5cae785ee1a553f0cf0184fadf7522
h3vanth <rhevanthraj@gmail.com>
2022-12-15 10:40:53 +0530

    feat(ProductDetails): show more details

--- src/components/dashboard/ProductDetails.tsx
+++ src/components/dashboard/ProductDetails.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
-import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+import Grid from "@mui/material/Grid";
+
 import { ProductData } from "./Product";
-import Add from "@mui/icons-material/Add";
 
 const ProductDetails: React.FC<{ product: ProductData | null }> = ({
   product,
@@ -14,16 +14,37 @@ const ProductDetails: React.FC<{ product: ProductData | null }> = ({
     alt = "product cover",
     isAvailable,
     productName,
+    price,
+    description,
+    availableQuantity,
   } = product ?? {};
   return (
     <Box sx={{ width: 450, textAlign: "center" }}>
       <img src={imageSource} alt={alt} style={{ width: 300, marginTop: 75 }} />
       <Typography variant="h3">{productName}</Typography>
-      <Chip
-        label={isAvailable ? "Available" : "Not available"}
-        color={isAvailable ? "success" : "error"}
-        variant="outlined"
-      />
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ py: 1 }}
+      >
+        <Grid item>
+          <Chip
+            label={
+              isAvailable ? `Available (${availableQuantity})` : "Not available"
+            }
+            color={isAvailable ? "success" : "error"}
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item>
+          <Chip label={`Price: ₹${price}`} color="info" variant="outlined" />
+        </Grid>
+      </Grid>
+      <Typography variant="body2">
+        {description || "No description available at the moment"}
+      </Typography>
     </Box>
   );
 };
