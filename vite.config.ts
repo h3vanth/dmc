c53a67156000cdf5ba724d6af43032bec1902402
h3vanth <rhevanthraj@gmail.com>
2023-01-12 21:39:59 +0530

    chore: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "c8",
+      reporter: ["text", "html"],
+      exclude: [
+        ...configDefaults.exclude,
+        "src/mocks/**",
+        "src/types/**",
+        "src/constants/**",
+        "__tests__/**",
+      ],
+    },
+  },
 });
